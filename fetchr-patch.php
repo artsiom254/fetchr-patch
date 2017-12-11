69ec88b0b440fa61a7f85adc0d9a1ddbe633da3f
Artem <artsiom.k@gmail.com>
2017-12-11 17:52:28 +0300

    add git info

--- fetchr-patch.php
+++ fetchr-patch.php
@@ -3,6 +3,7 @@
 Plugin Name: Fetchr patch express
 Description: Plugin for improving Fetchr shiping method
 Version: 1.0
+Plugin URI: https://github.com/artsiom254/fetchr-patch
 */
 $active_plugins = apply_filters('active_plugins', get_option('active_plugins'));
 if (in_array('woocommerce/woocommerce.php', $active_plugins) && in_array('fetchr/fetchr.php', $active_plugins)) {
